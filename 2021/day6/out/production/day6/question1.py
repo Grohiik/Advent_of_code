1ff869d8cc15195004d2df7a356178038ab43dcd
Heart <heartbrrrrr@protonmail.com>
2021-12-06 21:09:01 +0100

    day 6

---
+++ 2021/day6/out/production/day6/question1.py
@@ -0,0 +1,16 @@
+from input import *
+
+input = input.split(",")
+input = [int(i, base=10) for i in input]
+
+# question 1 = range 80, question 2 = range 256
+for j in range(80):
+    for i in range(len(input)):
+        if input[i] == 0:
+            input.append(8)
+            input[i] = 6
+        else:
+            input[i] -= 1
+    print(len(input))
+
+print(len(input))
