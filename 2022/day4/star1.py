6fe527c38a4609c1d1804fbd1291dea41daaf605
Linnea <heartbrrrrr@protonmail.com>
2022-12-04 11:42:47 +0100

    Day 4 2022
    My over reliance on list comprehension strikes again

---
+++ 2022/day4/star1.py
@@ -0,0 +1,14 @@
+from input import input
+
+input = input.split("\n")
+input = [i.split(",") for i in input]
+input = [left.split("-")+(right.split("-")) for left, right in input]
+input = [[int(i) for i in blub] for blub in input]
+
+output = 0
+
+for x1, x2, y1, y2 in input:
+    if ((x1 <= y1 and x2 >= y2) or (x1 >= y1 and x2 <= y2)):
+        output += 1
+
+print(output)
