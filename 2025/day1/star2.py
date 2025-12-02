5b5ec1418ad8faf029dec0a12cd4761488ae8a25
Linnéa Mörk <linnea.mork@gmail.com>
2025-12-02 14:49:41 +0100

    2025 day 1 star 2, non working

---
+++ 2025/day1/star2.py
@@ -0,0 +1,30 @@
+from input import input
+
+number = 0
+
+rotation = 50
+input = input.split("\n")
+
+for row in input:
+    way = row[0]
+    amount = row[1:]
+
+    if(way == "R"):
+        rotation += int(amount)
+    elif(way == "L"):
+        rotation -= int(amount)
+
+    while(rotation > 100):
+        number += 1
+        rotation -= 100
+
+    while(rotation < 0):
+        number += 1
+        rotation += 100
+
+    rotation %= 100
+    if(rotation == 0):
+        number+=1
+
+
+print(number)
